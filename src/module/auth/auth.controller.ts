7d0798f741e7cf9f59c65991e5d9bd5e419533fe
Felipe Destro <felipe.s.destro@gmail.com>
2026-04-04 11:04:10 -0400

    feat: forgot and reset password

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthLoginDTO } from './domain/dto/login-dto';
 import { AuthRegisterDTO } from './domain/dto/register-user-dto';
+import { AuthResetDTO } from './domain/dto/auth-reset-dto';
+import { AuthForgotPasswordDTO } from './domain/dto/auth-forgot-password-dto';
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +25,15 @@ export class AuthController {
   @Post('register')
   register(@Body() body: AuthRegisterDTO) {
     return this.authService.register(body);
+  }
+
+  @Patch('reset-password')
+  resetPassword(@Body() body: AuthResetDTO) {
+    return this.authService.reset(body);
+  }
+
+  @Post('forgot-password')
+  forgotPassword(@Body() email: AuthForgotPasswordDTO) {
+    return this.authService.forgotPassword(email);
   }
 }
--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -6,6 +6,9 @@ import { UserService } from '../users/user.service';
 import { AuthLoginDTO } from './domain/dto/login-dto';
 import { AuthRegisterDTO } from './domain/dto/register-user-dto';
 import { CreateUserDTO } from '../users/domain/dto/create-user.dto';
+import { AuthResetDTO } from './domain/dto/auth-reset-dto';
+import { ValidateTokenDTO } from './domain/dto/validate-token-dto';
+import { AuthForgotPasswordDTO } from './domain/dto/auth-forgot-password-dto';
 
 @Injectable()
 export class AuthService {
@@ -14,10 +17,13 @@ export class AuthService {
     private readonly userService: UserService,
   ) {}
 
-  async generateToken(user: User) {
+  async generateToken(
+    user: User,
+    expiresIn: JwtSignOptions['expiresIn'] = '1d',
+  ) {
     const payload = { sub: user.id, name: user.name };
     const options: JwtSignOptions = {
-      expiresIn: '1d',
+      expiresIn: expiresIn,
       issuer: 'cshotel',
       audience: 'users',
     };
@@ -30,7 +36,7 @@ export class AuthService {
   async login({ email, password }: AuthLoginDTO) {
     const user = await this.userService.findByEmail(email);
 
-    if (!user || (await bcrypt.compare(password, user.password))) {
+    if (!user || !(await bcrypt.compare(password, user.password))) {
       throw new UnauthorizedException('Email or password is incorrect');
     }
 
@@ -48,5 +54,45 @@ export class AuthService {
     const user = await this.userService.create(newUser);
 
     return await this.generateToken(user);
+  }
+
+  async reset({ token, password }: AuthResetDTO) {
+    const { valid, decoded } = await this.validateToken(token);
+
+    if (!valid || !decoded) throw new UnauthorizedException('Invalid token');
+
+    const user = await this.userService.update(Number(decoded!.sub), {
+      password,
+    });
+
+    return await this.generateToken(user);
+  }
+
+  async forgotPassword({ email }: AuthForgotPasswordDTO) {
+    const user = await this.userService.findByEmail(email);
+
+    if (!user) throw new UnauthorizedException('Email is incorrect');
+
+    return await this.generateToken(user, '30m');
+  }
+
+  private async validateToken(token: string): Promise<ValidateTokenDTO> {
+    try {
+      const decoded = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_SECRET,
+        issuer: 'cshotel',
+        audience: 'users',
+      });
+
+      return {
+        valid: true,
+        decoded,
+      };
+    } catch (error: any) {
+      return {
+        valid: false,
+        message: error,
+      };
+    }
   }
 }
